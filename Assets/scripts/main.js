2ce1a0be858dbd3c1703c5b613220d6adbb7513b
Eman Butt <butt.eman391@gmail.com>
2025-11-15 04:03:09 +0500

    Todo list page created.

--- Assets/scripts/main.js
+++ Assets/scripts/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $("#pomodoro").on("click", function(){
     window.location.href = "../../Assets/pages/pomodoro.html"
     });
+
+    $("#todo").on("click", function(){
+    window.location.href = "../../Assets/pages/todo-list.html"
+    });
 });
